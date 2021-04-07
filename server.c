b682d5f4a24a5548525168d8e56216f5acde47d9
nevilleomangi <31406314+nevilleomangi@users.noreply.github.com>
2021-04-07 15:01:02 +0300

    add iterative server

--- server.c
+++ server.c
@@ -38,24 +38,43 @@ int close(int acc_no); // returns 0 for success, -1 for failure
 const char* timestamp_to_str(long int n);
 
 int main(){
-	// char server_message[256]="You have reached the server!";
+	accounts = create(sizeof(Account));
 
-	// int server_socket;
-	// server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	
-	// struct sockaddr_in server_address;
-	// server_address.sin_family = AF_INET;
-	// server_address.sin_port = htons(9302);
-	// server_address.sin_addr.s_addr = INADDR_ANY;
+	int client_socket, server_socket, n;
+	struct sockaddr_in server_address;
+	char client_response[256], server_response[256];
 
-	// bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9302);
+	server_address.sin_addr.s_addr = INADDR_ANY;
 
-	// listen(server_socket,5);
+	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+	listen(server_socket, 8);
 
-	// int client_socket;
-	// client_socket = accept(server_socket, NULL, NULL);
-	// send(client_socket, server_message, sizeof(server_message), 6);
-	// close(client_socket);
+	for (;;) {
+		client_socket = accept(server_socket, NULL, NULL);
+    while ((n = recv(client_socket, &client_response, sizeof(client_response), 0)) > 0)  {
+			printf("Client has sent: %s\n", client_response);
+
+			ClientMessage clientMessage = parse_message(client_response);
+			process_message(clientMessage, server_response, sizeof(server_response));
+			send(client_socket, server_response, sizeof(server_response), 6);
+    }
+
+    if (n < 0) {
+      perror("Read error");
+      exit(1);
+    }
+
+    close(client_socket);
+  }
+
+	close(server_socket);
+
+	return 0;
+}
+
 
 	Account *a1;
 	Transaction *t1, *t2;
